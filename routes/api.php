4c74e00ed681fd5dd726f06aea215f651ed9e9aa
nabilasitir <nabilasitir@gmail.com>
2020-02-08 12:08:48 +0700

    modify some endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Cuaca;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
 });
 
